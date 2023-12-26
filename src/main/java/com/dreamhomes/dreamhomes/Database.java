ed5f457026901b6d178c8f1a97e321914f03207a
Mathias Wakgari <mathiaswakgari12@gmail.com>
2023-12-26 15:45:39 +0300

    Added another address and another home.

--- src/main/java/com/dreamhomes/dreamhomes/Database.java
+++ src/main/java/com/dreamhomes/dreamhomes/Database.java
@@ -60,7 +60,7 @@ public class Database {
                 "home_about LONGTEXT," +
                 "year_built INT NOT NULL," +
                 "home_type VARCHAR(100) NOT NULL," +
-                "home_utilities VARCHAR(100)," +
+                "home_utilities MEDIUMTEXT," +
                 "home_category VARCHAR(100)," +
                 "agent_name VARCHAR(100)," +
                 "agent_number VARCHAR(100) NOT NULL ," +
@@ -133,13 +133,13 @@ public class Database {
         try {
             System.out.println("Adding address...");
             PreparedStatement preparedStatement = connection.prepareStatement(query);
-            preparedStatement.setString(1,"President Obama");
-            preparedStatement.setString(2, "1600 Pennsylvania Avenue NW");
-            preparedStatement.setString(3, String.valueOf(NULL));
-            preparedStatement.setString(4, "Washington");
-            preparedStatement.setString(5, "DC");
+            preparedStatement.setString(1,"3463 Switchgrass St");
+            preparedStatement.setNull(2,NULL );
+            preparedStatement.setNull(3, NULL);
+            preparedStatement.setString(4, "Dacona");
+            preparedStatement.setString(5, "CO");
             preparedStatement.setString(6, "US");
-            preparedStatement.setString(7, "20500");
+            preparedStatement.setString(7, "80514");
 
             preparedStatement.execute();
             isInserted = true;
@@ -157,27 +157,29 @@ public class Database {
         try {
             System.out.println("Adding home...");
             PreparedStatement preparedStatement = connection.prepareStatement(query);
-            preparedStatement.setInt(1,1);
-            preparedStatement.setInt(2, 29331952);
-            preparedStatement.setInt(3, 4);
-            preparedStatement.setInt(4, 3);
-            preparedStatement.setInt(5, 214);
-            preparedStatement.setString(6, "Welcome to your future home in Dacono, CO! This splendid 4-bedroom, 3-bathroom 2-story house is a perfect blend of modern comfort and thoughtful design. As you enter, you'll be welcomed by an inviting open floor plan, showcasing the spaciousness and versatility of the living spaces. The eat-in kitchen is a focal point, boasting a kitchen island, pantry, and the convenience of a modern lifestyle. Relax and rejuvenate in the luxurious five-piece master bath, a private oasis within your home. The master suite is designed for tranquility and comfort, offering a retreat from the hustle and bustle of daily life. Imagine entertaining or simply enjoying meals on your new back patio. With mountain views as your backdrop, this outdoor space becomes a picturesque retreat. The perfect spot to unwind after a long day and create lasting memories with friends and family. Central air cooling ensures your comfort year-round, and ceiling fans add an extra touch of circulation and freshness. The new back patio extends your living space outdoors, creating a seamless connection between indoor and outdoor living. Roof replaced in 2018 with class 4 shingles as well as a new hot water heater in 2021! Exciting developments are on the horizon with a new King Soopers breaking ground in 2024, offering convenience for your shopping needs. This property is strategically located, providing easy access to schools and a short commute to I-25, making your daily routine more efficient. This home is not just a residence; it's an opportunity to embrace the Colorado lifestyle. With thoughtful features, mountain views, and the promise of nearby amenities, this property in Dacono is a gem waiting to be discovered. Don't miss the chance to make it your own. Schedule a showing today!");
-            preparedStatement.setInt(7, 2015);
+            preparedStatement.setInt(1,3);
+            preparedStatement.setInt(2, 33844560);
+            preparedStatement.setInt(3, 3);
+            preparedStatement.setInt(4, 2);
+            preparedStatement.setInt(5, 165);
+            preparedStatement.setString(6, "Welcome to your dream ranch home in the quaint Sweetgrass community! This stunning residence offers a blend of comfort, functionality, and timeless design. The open concept floor plan seamlessly combines the living, dining, and kitchen areas, providing an ideal space for both relaxation and entertainment. The living room features luxury vinyl plank and large windows, allowing an abundance of natural light to flood the room, creating a warm and welcoming atmosphere. Adjacent to the living area, you'll find a well-appointed kitchen with granite countertops, a giant island, and double stacked cabinets to provide ample storage for all of your culinary needs. Whether you're preparing a quick breakfast or hosting a dinner party, this kitchen will exceed your expectations. The home also offers three spacious bedrooms, each offering a peaceful sanctuary for rest and relaxation. The master suite features a generous layout, plush carpeting, walk-in closet and an en-suite bathroom. Downstairs, a full unfinished basement provides an area for endless possibilities, whether it be a home gym, extra storage space, or a future finished second floor! Outside, your backyard offers maximum privacy and is the perfect space for enjoying al fresco dining or simply unwinding in the fresh air. For car enthusiasts or those with a growing family, the two-car garage offers tons of space for parking vehicles and toys, storage, or even a workshop. This beautiful ranch home offers a blend of style, comfort, and space and you won't want to miss out! The Listing Team represents builder/seller as a Transaction Broker.");
+            preparedStatement.setInt(7, 2023);
             preparedStatement.setString(8, "Single Family");
             preparedStatement.setString(9, "Forced Air Heating and Cooling System\n" +
                     "High Speed Internet\n" +
-                    "Cable TV Available");
+                    "Cable TV Available\n" +
+                    "Natural Gas Connected\n" +
+                    "Phone Available");
             preparedStatement.setString(10,"sell" );
-            preparedStatement.setString(11,"Holly Vejrostek" );
-            preparedStatement.setString(12, "(720) 902-6509");
-            preparedStatement.setString(13,"https://images.homes.com/listings/215/2200550253-128155561/5625-westview-cir-dacono-co-primaryphoto.jpg" );
-            preparedStatement.setString(14,"https://images.homes.com/listings/115/9247170253-128155561/5625-westview-cir-dacono-co-buildingphoto-2.jpg" );
-            preparedStatement.setString(15,"https://images.homes.com/listings/115/1347170253-128155561/5625-westview-cir-dacono-co-buildingphoto-3.jpg" );
-            preparedStatement.setString(16,"https://images.homes.com/listings/115/8573410253-128155561/5625-westview-cir-dacono-co-buildingphoto-4.jpg" );
-            preparedStatement.setString(17,"https://images.homes.com/listings/115/9773410253-128155561/5625-westview-cir-dacono-co-buildingphoto-5.jpg" );
-            preparedStatement.setString(18,"https://images.homes.com/listings/115/3083410253-128155561/5625-westview-cir-dacono-co-buildingphoto-6.jpg" );
-            preparedStatement.setString(19,"https://images.homes.com/listings/115/1383410253-128155561/5625-westview-cir-dacono-co-buildingphoto-7.jpg" );
+            preparedStatement.setString(11,"Team Lassed" );
+            preparedStatement.setString(12, "(833) 833-5957");
+            preparedStatement.setString(13,"https://images.homes.com/listings/215/7273718153-112445561/3463-switchgrass-st-dacono-co-primaryphoto.jpg" );
+            preparedStatement.setString(14,"" );
+            preparedStatement.setString(15,"" );
+            preparedStatement.setString(16,"" );
+            preparedStatement.setString(17,"" );
+            preparedStatement.setString(18,"" );
+            preparedStatement.setString(19,"" );
 
             preparedStatement.execute();
             isInserted = true;
@@ -232,6 +234,10 @@ public class Database {
         }
     }
 
+    public static void main(String[] args) {
+        Database database = new Database();
+        database.insertIntoHomes();
+    }
 
 
 }
