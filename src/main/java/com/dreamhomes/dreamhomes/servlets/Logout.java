c61a81fcf01affff8941b50670ab851f10f06165
Mathias Wakgari <mathiaswakgari12@gmail.com>
2023-12-29 16:46:58 +0300

    Implemented Logout.java Servlet

---
+++ src/main/java/com/dreamhomes/dreamhomes/servlets/Logout.java
@@ -0,0 +1,21 @@
+package com.dreamhomes.dreamhomes.servlets;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+
+@WebServlet("/logout")
+public class Logout extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession httpSession = req.getSession(false);
+        httpSession.invalidate();
+
+        resp.sendRedirect("/");
+    }
+}
